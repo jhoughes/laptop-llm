05964cca0e3591b35d5f76419f947a9bf53c7930
Joe Houghes <joehoughes@gmail.com>
2025-04-08 00:17:15 +0000

    Add local RAG pipeline

---
+++ rag_pipeline/install.sh
@@ -0,0 +1,19 @@
+sudo apt install python3-pip
+pip3 install -r requirements.txt
+sudo apt update > /dev/null 2>&1
+sudo apt install poppler-utils
+sudo apt update && sudo apt install -y tesseract-ocr
+sudo apt update && sudo apt install -y libgl1
+docker run --name pgvector-container -e POSTGRES_USER=langchain -e POSTGRES_PASSWORD=langchain -e POSTGRES_DB=langchain -p 6024:5432 -d pgvector/pgvector:pg16
+curl -fsSL https://packages.redis.io/gpg | sudo gpg --dearmor -o /usr/share/keyrings/redis-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/redis-archive-keyring.gpg] https://packages.redis.io/deb $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/redis.list
+sudo apt-get update > /dev/null 2>&1
+sudo apt-get install lsb-release curl gpg
+curl -fsSL https://packages.redis.io/gpg | sudo gpg --dearmor -o /usr/share/keyrings/redis-archive-keyring.gpg
+sudo chmod 644 /usr/share/keyrings/redis-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/redis-archive-keyring.gpg] https://packages.redis.io/deb $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/redis.list
+sudo apt-get update > /dev/null 2>&1
+sudo apt-get install redis-server  > /dev/null 2>&1
+sudo systemctl enable redis-server
+sudo systemctl start redis-server
+sudo apt update && sudo apt install -y postgresql postgresql-client
---
+++ rag_pipeline/virtual_environment.sh
@@ -0,0 +1,10 @@
+python3 -m venv ./venv
+source ./venv/bin/activate
+pip3 install langchain langchain-community python_dotenv
+pip3 install langchain-openai
+pip3 install pandas numpy
+pip3 install streamlit
+pip3 install "unstructured[all-docs]<=0.16.10"
+pip3 install langchain_postgres
+pip3 install "redis>=4.1.0"
+pip3 install ipywidgets
